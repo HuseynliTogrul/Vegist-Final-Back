7a8f9a79b1a29da0514142d5c269ca714f209508
HuseynliTogrul <toghrulih@code.edu.az>
2024-06-04 17:02:30 +0400

    back

---
+++ Vegist/Controllers/BlogBrandController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vegist.Controllers
+{
+    public class BlogBrandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
